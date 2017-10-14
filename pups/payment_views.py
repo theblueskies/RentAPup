8068e79af0db4515bb35809af6064c1f1aa02c4c
Dana K. Engebretson <dkengebretson@spscommerce.com>
2017-10-14 13:23:42 -0500

    added stripe integration

--- pups/payment_views.py
+++ pups/payment_views.py
@@ -1,1 +1,17 @@
-# Add the payment view here.
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+from django.shortcuts import render
+ 
+from pups.models import Sale
+from pups.forms import SalePaymentForm
+ 
+def charge(request):
+    if request.method == "POST":
+        form = SalePaymentForm(request.POST)
+ 
+        if form.is_valid(): # charges the card
+            return HttpResponse("Success! We've charged your card!")
+    else:
+        form = SalePaymentForm()
+ 
+    return render(request, "pups/charge.html", {'form': form})
--- pups/urls.py
+++ pups/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 
 from . import views
+from . import payment_views
 
 urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),
+    url(r'^charge/$', payment_views.charge, name="charge"),
 ]
