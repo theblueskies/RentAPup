d105c84eb969dd9b54529eee3ccb0240beb87e75
Blade <druhin.bala@gmail.com>
2017-10-14 18:13:44 -0500

    environment var settings

--- rentapup/settings.py
+++ rentapup/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-ENVIRONMENT = os.getenv('ENVIRONMENT', 'dev')
+ENVIRONMENT = os.getenv('ENVIRONMENT', 'prod')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -75,7 +75,6 @@ WSGI_APPLICATION = 'rentapup.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-print ('ENVIRONMENT ', ENVIRONMENT)
 db_options = {
     'dev': {
         'ENGINE': 'django.db.backends.sqlite3',
