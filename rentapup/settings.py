3ef875ec2c725b48b08061a2ce8d77a49843ee10
Blade <druhin.bala@gmail.com>
2017-10-15 01:09:00 -0500

    more static pain

--- rentapup/settings.py
+++ rentapup/settings.py
@@ -139,9 +139,9 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(PROJECT_ROOT, 'static'),
+# )
 
 # STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
