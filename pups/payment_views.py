1b30740bef0cb4e89baa0d7a3ba0c4354fc7d621
Dana K. Engebretson <dkengebretson@spscommerce.com>
2017-10-15 11:14:30 -0500

    working payment

--- pups/payment_views.py
+++ pups/payment_views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.template import loader
 import stripe
 
 from pups import stripe_settings
@@ -9,34 +11,39 @@ stripe.api_key = stripe_settings.STRIPE_SECRET_KEY
 def payment_form(request):
 
     context = { "stripe_key": stripe_settings.STRIPE_PUBLIC_KEY }
-    return render(request, "payment_form.html", context)
+    return render(request, "pups/payment_form.html", context)
 
 def checkout(request):
 
+    """
     new_puppy = Puppy(
         name = "Blackjack",
         breed  = "Lab",
-        age = "1"
+        age = "1",
+        owner = 
     )
+    """
 
     if request.method == "POST":
         token = request.POST.get("stripeToken")
 
     try:
         charge = stripe.Charge.create(
-            amount = 1,
+            amount = 50,
             currency = "usd",
             source = token,
             description = "The product charged to the user"
         )
 
-        new_puppy.charge_id = charge.id
+        # new_puppy.charge_id = charge.id
 
     except stripe.error.CardError as ce:
         return False, ce
 
     else:
-        new_puppy.save()
-        return redirect("thank_you_page")
+        # new_puppy.save()
+        template = loader.get_template('pups/checkout.html')
+        return HttpResponse(template.render({}, request))
+        # return redirect("pups/thank_you_page")
         # The payment was successfully processed, the user's card was charged.
         # You can now redirect the user to another page or whatever you want
--- pups/urls.py
+++ pups/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # ex: /polls/
     url(r'^$', views.index, name='index'),
     url(r"^checkout$", payment_views.checkout, name="checkout"),
-    url(r"^payment_form$", payment_views.payment_form, name="payment_form")
+    url(r"^payment_form$", payment_views.payment_form, name="payment_form"),
     url(r'^user-profile/$', views.user_profile, name='user_profile'),
     url(r'^edit-profile/$', views.edit_profile, name='edit_profile'),
     url(r'^home/$', views.home, name='home'),
